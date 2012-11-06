044f379a764040ebe0c09773d769505e452324bd
Samuel Rinnetmaki <samuel@blip.at>
2012-11-06 21:58:19 +0200

    added line/dir parameters

--- app.py
+++ app.py
@@ -17,13 +17,14 @@ yd = weekdays[lt[6]]
 
 app = Flask(__name__)
 
-@app.route('/')
-
-# print live_api_url
-
-def index():
-  feed = urlopen(live_api_url)
-
+@app.route('/', defaults={'line': '', 'dir': ''})
+@app.route('/<line>/<int:dir>')
+def index(line, dir):
+  url = live_api_url
+  if (line and dir):
+    url += '&lines=' + line + '.' + str(dir)
+  print url
+  feed = urlopen(url)
   msg = gtfs_realtime_pb2.FeedMessage()
   msg.header.gtfs_realtime_version = "1.0"
   msg.header.incrementality = msg.header.FULL_DATASET
@@ -33,9 +34,12 @@ def index():
   for line in feed:
     # print line
     row = line.strip().split(';')
+    if (len(row) < 4):
+      print line
+      continue
     if ((row[2] == 0) or (row[3] == 0)):
       continue;
-    print row
+    # print row
     ent = msg.entity.add()
     ent.id = str(cnt)
     day = wd
@@ -59,5 +63,5 @@ def index():
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
+  app.debug = True
   app.run(host='0.0.0.0', port=port)
-
