c4aed20f8c840bb4ea5223cc055eb8308bce57b1
Samuel Rinnetmaki <samuel@blip.at>
2012-11-28 13:42:31 +0200

    correct longitude

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ from urllib import urlopen
 import time
 import os
 
-live_api_url = 'http://83.145.232.209:10001/?type=vehicles' + \
+# prod
+api_ip = '83.145.232.209'
+
+#test
+# api_ip = '213.157.92.101'
+
+live_api_url = 'http://' + api_ip + ':10001/?type=vehicles' + \
                '&lng1=23&lat1=60&lng2=26&lat2=61&online=1'
 gtfs_version = '20120224'
 weekdays = ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe', 'La', 'Su']
@@ -23,7 +29,7 @@ def index(line, dir):
   url = live_api_url
   if (line and dir):
     url += '&lines=' + line + '.' + str(dir)
-  print url
+  # print url
   feed = urlopen(url)
   msg = gtfs_realtime_pb2.FeedMessage()
   msg.header.gtfs_realtime_version = "1.0"
@@ -35,7 +41,7 @@ def index(line, dir):
     # print line
     row = line.strip().split(';')
     if (len(row) < 4):
-      print line
+      # print line
       continue
     if ((row[2] == 0) or (row[3] == 0)):
       continue;
@@ -55,7 +61,7 @@ def index(line, dir):
     ent.vehicle.vehicle.id = row[0]
     ent.vehicle.vehicle.label = row[1]
     ent.vehicle.position.latitude = float(row[2])
-    ent.vehicle.position.longitude = float(row[2])
+    ent.vehicle.position.longitude = float(row[3])
     ent.vehicle.position.bearing = int(row[4])
 
   # print(msg);
